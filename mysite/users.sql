20cae1be4fe160008bc4366937e8681a0c4ffb47
Austin Sims <sims4@purdue.edu>
2014-04-10 19:49:18 -0400

    whoops

--- mysite/users.sql
+++ mysite/users.sql
@@ -1,5 +1,5 @@
-INSERT INTO table VALUES(1,'pbkdf2_sha256$10000$jyjJQz9hOWQV$p9gmEJ715kJKXUvGDIMO9Vps85MZpkL60pp6x8hJK4A=','2014-04-10 22:31:21.532993',1,'root','Richard','Stallman','',1,1,'2014-04-09 21:31:14');
-INSERT INTO table VALUES(2,'pbkdf2_sha256$10000$NOmVZ6d35qBB$ScPmsIdT0JElG9Ho9HL21yVJPFN0YMyLQzHOiUcvVXM=','2014-04-10 22:48:21.352200',0,'asims','Austin','Sims','',0,1,'2014-04-09 22:32:44');
-INSERT INTO table VALUES(3,'pbkdf2_sha256$10000$DklhDYSz6EH6$R9yjtbY3TFGuIbCqJLpRyat2u6jLWQOif4w15CiH3QU=','2014-04-09 22:39:21.052032',0,'cbell','Collin','Bell','',0,1,'2014-04-09 22:33:20');
-INSERT INTO table VALUES(4,'pbkdf2_sha256$10000$OVPmYPk1jvZW$SOTfYgkmq9l0MAwqSZagVRnJVuWIXwxjvU/VOMHRoWw=','2014-04-09 22:38:38.297034',0,'karma','Robert','Sakhofanoskiavichsky','',0,1,'2014-04-09 22:33:48');
-INSERT INTO table VALUES(5,'pbkdf2_sha256$10000$ivR3iKg4NLFD$DButA33qjnW5ZmFYyMbfwfdkgRxPWY/GUgxWNIwpZ6M=','2014-04-09 22:39:45',0,'elgatomantocko','Elliott','Mantock','',0,1,'2014-04-09 22:34:27');
+INSERT INTO auth_user VALUES(1,'pbkdf2_sha256$10000$jyjJQz9hOWQV$p9gmEJ715kJKXUvGDIMO9Vps85MZpkL60pp6x8hJK4A=','2014-04-10 22:31:21.532993',1,'root','Richard','Stallman','',1,1,'2014-04-09 21:31:14');
+INSERT INTO auth_user VALUES(2,'pbkdf2_sha256$10000$NOmVZ6d35qBB$ScPmsIdT0JElG9Ho9HL21yVJPFN0YMyLQzHOiUcvVXM=','2014-04-10 22:48:21.352200',0,'asims','Austin','Sims','',0,1,'2014-04-09 22:32:44');
+INSERT INTO auth_user VALUES(3,'pbkdf2_sha256$10000$DklhDYSz6EH6$R9yjtbY3TFGuIbCqJLpRyat2u6jLWQOif4w15CiH3QU=','2014-04-09 22:39:21.052032',0,'cbell','Collin','Bell','',0,1,'2014-04-09 22:33:20');
+INSERT INTO auth_user VALUES(4,'pbkdf2_sha256$10000$OVPmYPk1jvZW$SOTfYgkmq9l0MAwqSZagVRnJVuWIXwxjvU/VOMHRoWw=','2014-04-09 22:38:38.297034',0,'karma','Robert','Sakhofanoskiavichsky','',0,1,'2014-04-09 22:33:48');
+INSERT INTO auth_user VALUES(5,'pbkdf2_sha256$10000$ivR3iKg4NLFD$DButA33qjnW5ZmFYyMbfwfdkgRxPWY/GUgxWNIwpZ6M=','2014-04-09 22:39:45',0,'elgatomantocko','Elliott','Mantock','',0,1,'2014-04-09 22:34:27');
